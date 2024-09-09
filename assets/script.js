acfce5dfef22430cb1da403f9cbec65b441d89af
Sofiane GAGAA <sofianegagaa@orange.fr>
2024-09-09 13:41:08 +0200

    improve script for indentation, naming, selector, JS Doc & comments

--- assets/script.js
+++ assets/script.js
@@ -3,61 +3,74 @@
 //! ********** CONSTANTS  **********
 
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"text":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"text":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"text":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"text":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+  {
+    "image": "slide1.jpg",
+    "text": "Impressions tous formats <span>en boutique et en ligne</span>"
+  },
+  {
+    "image": "slide2.jpg",
+    "text": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+  },
+  {
+    "image": "slide3.jpg",
+    "text": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+  },
+  {
+    "image": "slide4.png",
+    "text": "Autocollants <span>avec découpe laser sur mesure</span>"
+  }
 ]
 
-
 const image = document.querySelector(".banner-img");
-  // console.log(image);
 const text = document.querySelector("#banner p");
-  // console.log(texte);
 
-const leftArrowElement = document.querySelector(".arrow_left"); // const arrowLeft = document.querySelector(".arrow_left");
-  // console.log(arrowLeft);
-const rightArrowElement = document.querySelector(".arrow_right"); // const arrowRight = document.querySelector(".arrow_right");
-  // console.log(arrowRight);
+const leftArrowElement = document.querySelector(".arrow_left");
+const rightArrowElement = document.querySelector(".arrow_right");
 
-const dots = document.querySelectorAll(".dots");
-const Length = slides.length;
-  console.log(Length);
+const dots = document.querySelector(".dots");
+
+const LENGTH = slides.length;
 const URL = "./assets/images/slideshow/";
 
 //! ********** VARIABLES **********
-let i = 0; // index du slide courant
-let allDots ;
+
+let currentSlide = 0;
+let allDots;
 
 //! ********** FUNCTIONS **********
 
-/***** Generating four "dot"s *****/
+/**
+ * Mise à jour de la classe "dot_selected" sur le élément qui correspond à l'index i.
+ * Supprime la classe "dot_selected" sur tous les éléments de la liste allDots.
+ * Ajoute la classe "dot_selected" sur l'élément de la liste allDots correspondant à l'index i.
+ */
 const updateDots = () => {
-  for (let j = 0; j < allDots.length; j++) {
-    allDots[j].classList.remove("dot_selected");  
+  for (let j = 0; j < LENGTH; j++) {
+    allDots[j].classList.remove("dot_selected");
   }
-  allDots[j].classList.add("dot_selected");
+  allDots[currentSlide].classList.add("dot_selected");
 }
 
+/**
+ * Mise à jour du slider en fonction de l'index i.
+ * Met à jour l'image et le texte du slider.
+ * Appelle updateDots pour mettre à jour les dots.
+ */
 const updateSlider = () => {
-  image.setAttribute("src", URL + slides[i].image);
-  text.innerHTML = slides[i].text;
+  image.setAttribute("src", URL + slides[currentSlide].image);
+  text.innerHTML = slides[currentSlide].text;
 
   updateDots();
 }
 
+/**
+ * Génère les éléments "dots" du slider.
+ * Crée un élément "span" pour chaque slide dans le tableau slides.
+ * Ajoute la classe "dot" à chaque élément.
+ * Ajoute chaque élément à la liste des enfants de l'élément "dots".
+ * Stocke la liste des éléments "dots" dans la variable allDots.
+ * Ajoute la classe "dot_selected" au premier élément de la liste allDots.
+ */
 const addDots = () => {
   slides.forEach(() => {
     const dot = document.createElement("span");
@@ -68,46 +81,41 @@ const addDots = () => {
   document.querySelector(".dot").classList.add("dot_selected");
 }
 
-/***** Generating four "dot"s *****/
+/**
+ * Décrémente l'index i de 1 et met à jour l'image et le texte du diaporama.
+ * L'index revient à la fin du tableau slides si il devient négatif.
+ * Les points de navigation du diaporama sont également mis à jour pour refléter le nouvel index.
+ */
 
-/***** fonction de mise à jour du slider *****/
-
-
-/***** fonction de navigation dans un slider *****/
 const slideLeft = () => {
-  i = (i + Length -1) % Length;
-
-  updateSlider();   
-}
-const slideRight = () => {
-  i = (i + 1) % Length;
+  currentSlide = (currentSlide + LENGTH - 1) % LENGTH;
 
   updateSlider();
 }
 
-const addListeners = () => {
-rightArrowElement.addEventListener("click", () => slideRight());
-leftArrowElement.addEventListener("click", () => slideLeft());
+/**
+ * Increments the index i by 1, and updates the slider image and text.
+ * The index wraps around to the start of the slides array if it becomes equal to the length of the array.
+ * The slider dots are also updated to reflect the new index.
+ */
+const slideRight = () => {
+  currentSlide = (currentSlide + 1) % LENGTH;
+
+  updateSlider();
 }
 
+/**
+ * Adds two event listeners to the left and right arrow elements.
+ * The left arrow element listener calls the slideLeft function when clicked.
+ * The right arrow element listener calls the slideRight function when clicked.
+ */
+const addListeners = () => {
+  rightArrowElement.addEventListener("click",  slideRight);
+  leftArrowElement.addEventListener("click",  slideLeft);
+}
 
-/*** fonction updateStyle de la mise à jour des dots de la diapositive active ***/
-
-
-
-/*** fonction updateSlider de la mise à jour de l'image de la diapositive active ***/
-
-
-/*** fonction updateText de la mise à jour du texte de la diapositive active ***/
 //! ********** MAIN **********
+
 addListeners();
 addDots();
-updateSlider(); // Initialisation et mise à jour du slider
-
-
-
-// for (let i = 0; i < slides.length; i++) {
-//   let newElement = document.createElement("div");
-//   newElement.classList.add("dot");
-//   document.querySelector(".dots").appendChild(newElement);
-// }
+updateSlider();
