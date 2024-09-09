ac5b15558e72129a039ab34bc00e3dff0d3712c6
Sofiane GAGAA <sofianegagaa@orange.fr>
2024-09-09 12:52:12 +0200

    Fix script arrows and dots

--- assets/script.js
+++ assets/script.js
@@ -24,67 +24,90 @@ const slides = [
 
 const image = document.querySelector(".banner-img");
   // console.log(image);
-const texte = document.querySelector("#banner p");
+const text = document.querySelector("#banner p");
   // console.log(texte);
 
-const arrowLeft = document.querySelector(".arrow_left");
+const leftArrowElement = document.querySelector(".arrow_left"); // const arrowLeft = document.querySelector(".arrow_left");
   // console.log(arrowLeft);
-const arrowRight = document.querySelector(".arrow_right");
+const rightArrowElement = document.querySelector(".arrow_right"); // const arrowRight = document.querySelector(".arrow_right");
   // console.log(arrowRight);
 
-
-/***** Generating four "dot"s *****/
-for (let i = 0; i < slides.length; i++) {
-  let newElement = document.createElement("div");
-  newElement.classList.add("dot");
-  document.querySelector(".dots").appendChild(newElement);
-}
-
-
-
-
-
-
-const LENGTH = slides.length ;
+const dots = document.querySelectorAll(".dots");
+const Length = slides.length;
+  console.log(Length);
 const URL = "./assets/images/slideshow/";
-  console.log(URL);
-
-
-
-const text = document.querySelector("#banner p");
-  console.log(text);
-const dots = document.querySelector(".dots");
-  console.log(dots);
 
 //! ********** VARIABLES **********
-
-let  i = 0 ;
+let i = 0; // index du slide courant
+let allDots ;
 
 //! ********** FUNCTIONS **********
 
-const slideLeft = () => (i - 1 + LENGTH) % LENGTH;
-const slideRight = () => (i + 1) % LENGTH;
+/***** Generating four "dot"s *****/
+const updateDots = () => {
+  for (let j = 0; j < allDots.length; j++) {
+    allDots[j].classList.remove("dot_selected");  
+  }
+  allDots[j].classList.add("dot_selected");
+}
+
+const updateSlider = () => {
+  image.setAttribute("src", URL + slides[i].image);
+  text.innerHTML = slides[i].text;
+
+  updateDots();
+}
+
+const addDots = () => {
+  slides.forEach(() => {
+    const dot = document.createElement("span");
+    dot.classList.add("dot");
+    dots.appendChild(dot);
+  });
+  allDots = document.getElementsByClassName("dot");
+  document.querySelector(".dot").classList.add("dot_selected");
+}
+
+/***** Generating four "dot"s *****/
+
+/***** fonction de mise à jour du slider *****/
+
+
+/***** fonction de navigation dans un slider *****/
+const slideLeft = () => {
+  i = (i + Length -1) % Length;
+
+  updateSlider();   
+}
+const slideRight = () => {
+  i = (i + 1) % Length;
+
+  updateSlider();
+}
 
 const addListeners = () => {
-  arrowLeft.addEventListener("click", slideLeft);
-  arrowRight.addEventListener("click", slideRight);
+rightArrowElement.addEventListener("click", () => slideRight());
+leftArrowElement.addEventListener("click", () => slideLeft());
 }
+
 
 /*** fonction updateStyle de la mise à jour des dots de la diapositive active ***/
 
-const updatedots = () => {
-  for (let j = 0; j < LENGTH; j++) {
-    dots.children[j].classList.remove("active");
-  }
-  allDots[i].classList.add("active");
-}
+
 
 /*** fonction updateSlider de la mise à jour de l'image de la diapositive active ***/
 
 
 /*** fonction updateText de la mise à jour du texte de la diapositive active ***/
 //! ********** MAIN **********
-
 addListeners();
-// addDots();
+addDots();
 updateSlider(); // Initialisation et mise à jour du slider
+
+
+
+// for (let i = 0; i < slides.length; i++) {
+//   let newElement = document.createElement("div");
+//   newElement.classList.add("dot");
+//   document.querySelector(".dots").appendChild(newElement);
+// }
